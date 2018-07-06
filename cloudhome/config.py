827ca82bbe56829117adca24663f244fed9d1d80
Jordan McQueen <jordanmq@amazon.com>
2018-07-05 21:58:23 -0700

    fill out Config concept

--- cloudhome/config.py
+++ cloudhome/config.py
@@ -1,4 +1,13 @@
+import os
+
+MANIFEST_FILENAME = 'manifest.json'
+
 class Config:
     def __init__(self, data):
-        self.buckets = data.get('buckets', [])
+        self.cloudhome = os.path.expanduser(data.get('cloudhome', None))
+        self.bucket_names = data.get('bucket_names', [])
+        self.log_file = os.path.expanduser(data.get('log_file', '/tmp/cloudhome.log'))
+        self.credential_profile = data.get('credential_profile')
 
+    def bucket_manifests(self):
+        return [os.path.join(self.cloudhome, x, MANIFEST_FILENAME) for x in self.bucket_names]
