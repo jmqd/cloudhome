01860a0469e611473aeaf8374c87b98c958c708c
Jordan McQueen <jordanmq@amazon.com>
2018-07-05 21:56:31 -0700

    design iteration: extensibility, modularity
    1. Added ability to sync multiple buckets.
    2. Pushed configuration values to config file.
    3. Cleaned up naming/factored units of code.
    4. Integrated with concept of Config object.
    5. Improved logging format and coverage.
    6. Changed file structure, incl. manifest(s)

--- cloudhome/cloudhome.py
+++ cloudhome/cloudhome.py
@@ -1,53 +1,74 @@
 #!/usr/bin/env python3
 import boto3
+from botocore.exceptions import EndpointConnectionError
 import logging
 import hashlib
 import time
-import config
+from config import Config
 from calendar import timegm
 import json
 import os
 
-CLOUDHOME_DIR = os.path.expanduser(os.environ.get("CLOUDHOME"))
-LOCAL_DIR = os.path.join(CLOUDHOME_DIR, os.environ.get("CLOUDHOME_BUCKET_NAME"))
-CLOUDHOME_CONFIG = os.path.join(CLOUDHOME_DIR, "config.json")
-LOCAL_MANIFEST = os.path.join(LOCAL_DIR, ".manifest.json")
+CLOUDHOME_CONFIG = os.path.expanduser("~/.cloudhome.json")
 LOG_FILENAME = "/tmp/cloudhome.log"
 
 
-session = boto3.Session(profile_name = 'mcqueen.jordan')
-
 def main():
-    configure_logging()
+    config = Config(read_json(CLOUDHOME_CONFIG))
+    configure_logging(config.log_file)
+    session = boto3.Session(profile_name = config.credential_profile)
     s3 = session.client('s3')
-    manifest = read_json(LOCAL_MANIFEST)
+
+    bucket_manifest_filenames = config.bucket_manifests()
+    logging.info("Opened config file {}, proceeding to sync {} buckets".format(
+        CLOUDHOME_CONFIG, len(bucket_manifest_filenames)))
+
+    for bucket_manifest in (read_json(fn) for fn in bucket_manifest_filenames):
+        sync_bucket(s3, bucket_manifest)
+
+    logging.info("Finished syncing...".format(CLOUDHOME_CONFIG))
+
 
 def sync_bucket(s3, manifest):
-    logging.info("Beginning synchronization for {}".format(manifest.get('local_dir')))
-    conditional_bidirectional_sync(s3, manifest)
-    logging.info("Finished synchronization for {}".format(manifest.get('local_dir')))
+    logging.info("Beginning sync for {}".format(manifest.get('root')))
 
-def configure_logging():
-    logging.basicConfig(filename=LOG_FILENAME, level = logging.INFO)
+    conditional_bidirectional_sync(s3, manifest)
+
+    logging.info("Done with sync for {}".format(manifest.get('root')))
+
+
+def configure_logging(log_file):
+    logging.basicConfig(
+        filename = log_file,
+        level = logging.INFO,
+        format = '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+    )
 
 
 def conditional_bidirectional_sync(s3, manifest):
     bucket = manifest['bucket_name']
-    for k, v in manifest['items'].items():
-        sync_down_metadata(s3, manifest, k, v, bucket)
+    root = os.path.expanduser(manifest.get('root'))
+    manifest_filename = os.path.join(root, "manifest.json")
 
-        if v['local_etag'] == v['s3_metadata']['etag']:
-            logging.info("Short circuiting: The remote and local hashes for {} were equal.".format(k))
+    for key, metadata in manifest['items'].items():
+        sync_down_metadata(s3, manifest, key, metadata, bucket, root, manifest_filename)
+        record_local_stats(root, key, metadata)
+
+        if metadata.get('local_etag') == metadata['s3_metadata']['etag']:
+            logging.info("Short circuiting: The remote and local hashes for {} were equal.".format(key))
         else:
-            sync_file_down_if_stale(s3, k, v, bucket)
-            sync_file_up_if_newer(s3, k, v, bucket)
+            sync_file_down_if_stale(s3, key, metadata, bucket, manifest['root'])
+            sync_file_up_if_newer(s3, key, metadata, bucket)
 
-        logging.info("Completed bi-directional sync for {}".format(k))
+        logging.info("Completed bi-directional sync for {}".format(key))
 
 
-def sync_down_metadata(s3, manifest, k, v, bucket):
+def sync_down_metadata(s3, manifest, k, v, bucket, root, manifest_filename):
     try:
         metadata = s3.head_object(Bucket = bucket, Key = k)
+    except EndpointConnectionError as e:
+        logging.error("Cannot make a connection: {}".format(e))
+        return
     except Exception as e:
         logging.error("Issue HEADing {} from {}; error {}, code {}".format(k, bucket, e, e.response['Error']['Code']))
         return
@@ -60,20 +81,21 @@ def sync_down_metadata(s3, manifest, k, v, bucket):
 
     if latest_metadata != v.get('s3_metadata', None):
         v['s3_metadata'] = latest_metadata
-        write_json(manifest, LOCAL_MANIFEST)
+        write_json(manifest, manifest_filename)
         logging.info("Wrote new manifest data for {}.".format(k))
     else:
         logging.info("Metadata unchanged for {}. Wrote nothing locally.".format(k))
 
-    local_stats = os.stat(os.path.join(LOCAL_DIR, k))
-    v['local_last_modified'], v['local_size'] = local_stats.st_mtime, local_stats.st_size
-    v['local_etag'] = calculate_local_etag(os.path.join(LOCAL_DIR, k))
+def record_local_stats(root, key, metadata):
+    local_stats = os.stat(os.path.join(root, key))
+    metadata['local_last_modified'], metadata['local_size'] = local_stats.st_mtime, local_stats.st_size
+    metadata['local_etag'] = calculate_local_etag(os.path.join(root, key))
 
 
-def sync_file_down_if_stale(s3, k, v, bucket):
-    if v['local_last_modified'] < v['s3_metadata']['last-modified']:
+def sync_file_down_if_stale(s3, k, v, bucket, root):
+    if v.get('local_last_modified', 0) < v['s3_metadata']['last-modified']:
         try:
-            object = s3.download_file(bucket, k, os.path.join(LOCAL_DIR, k))
+            object = s3.download_file(bucket, k, os.path.join(root, k))
             logging.info("Downloaded object {}. Remote timestamp is {}, local was {}.".format(
                 k, v['s3_metadata']['last-modified'], v['local_last_modified']))
         except Exception as e:
@@ -82,11 +104,11 @@ def sync_file_down_if_stale(s3, k, v, bucket):
 
 def sync_file_up_if_newer(s3, k, v, bucket):
     remote_modified_at = v['s3_metadata']['last-modified']
-    local_modified_at = v['local_last_modified']
+    local_modified_at = v.get('local_last_modified', 0)
 
     if remote_modified_at < local_modified_at:
         try:
-            s3.upload_file(os.path.join(LOCAL_DIR, k), bucket, k)
+            s3.upload_file(os.path.join(manifest['root'], k), bucket, k)
             logging.info("Uploaded new file: {} had been locally updated.".format(k))
         except Exception as e:
             logging.error("Problem uploading {}: {}".format(k, e))
