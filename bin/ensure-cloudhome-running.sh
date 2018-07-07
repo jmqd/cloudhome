a4fa2876cd264eaa61a340a7f740e07252d61528
Jordan McQueen <jordanmq@amazon.com>
2018-07-06 22:25:44 -0700

    fix discrepancy between process and makerun

--- bin/ensure-cloudhome-running.sh
+++ bin/ensure-cloudhome-running.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 PYTHON_EXECUTABLE_PATH=$1
-PROCESS="${PYTHON_EXECUTABLE_PATH} ~/src/cloudhome/cloudhome/cloudhome.py"
+MAKERUN="${PYTHON_EXECUTABLE_PATH} ~/src/cloudhome/cloudhome/cloudhome.py"
+PROCESS="cloudhome.py"
 
 if ps ax | grep -v grep | grep "$PROCESS" > /dev/null
 then
     exit
 else
-    eval "${PROCESS} &"
+    eval "${MAKERUN} &"
 fi
 
 exit
