3729f4531e9167a68479a40da93be4265ab43264
Jordan McQueen <jordanmq@amazon.com>
2018-12-28 21:39:47 -0800

    Sync Manifest; Refactors; Increase Hertz of Sync
    - Small refactorings here and there
    - Indepently sync the manifest so that new files are
      picked up automagically
    - Increase hertz of sync from 1/15 to 1/5.

--- cloudhome/cloudhome.py
+++ cloudhome/cloudhome.py
@@ -7,10 +7,12 @@ import sys
 from config import Config
 from calendar import timegm
 import json
+import watchdog
 import os
 
 CLOUDHOME_CONFIG = os.path.expanduser("~/.cloudhome.json")
 LOG_FILENAME = "/tmp/cloudhome.log"
+SYNC_FREQUENCY_IN_HERTZ = 1/5
 
 
 def main():
@@ -20,7 +22,9 @@ def main():
 def continuously_sync():
     while True:
         sync_cloudhome()
-        time.sleep(15)
+
+        # we get the inverse of the hertz to determine the seconds to sleep
+        time.sleep(int(SYNC_FREQUENCY_IN_HERTZ ** -1))
 
 
 def sync_cloudhome():
@@ -48,11 +52,9 @@ def sync_all_buckets(s3, bucket_manifest_filenames):
 
 
 def sync_bucket(s3, manifest):
-    logging.info("Beginning sync for {}".format(manifest.get('root')))
-
+    logging.debug("Beginning sync for {}".format(manifest.get('root')))
     conditional_bidirectional_sync(s3, manifest)
-
-    logging.info("Done with sync for {}".format(manifest.get('root')))
+    logging.debug("Done with sync for {}".format(manifest.get('root')))
 
 
 def configure_logging(log_file):
@@ -67,49 +69,86 @@ def conditional_bidirectional_sync(s3, manifest):
     bucket = manifest['bucket_name']
     root = os.path.expanduser(manifest.get('root'))
     manifest_filename = os.path.join(root, "manifest.json")
+    sync_manifest(manifest_filename, bucket, s3, root)
 
     for key, metadata in manifest['items'].items():
         sync_down_metadata(s3, manifest, key, metadata, bucket, root, manifest_filename)
-        record_local_stats(root, key, metadata)
 
-        if metadata.get('local_etag') == metadata['s3_metadata']['etag']:
-            logging.info("Short circuiting: The remote and local hashes for {} were equal.".format(key))
+        # small weirdness here -- can pull false metadata info from s3 (refreshing manfiest)
+        # then "correct" it here. This is necessary regardless, but important to call out the
+        # abstract "race condition"
+        record_local_stats(root, key, metadata)
+        bidirectionally_sync_file(key, metadata, bucket, root, s3)
+
+
+def bidirectionally_sync_file(key, metadata, bucket, root, s3):
+        if remote_and_local_hashes_are_equal(metadata):
+            logging.debug("Short circuiting: The remote and local hashes for {} were equal.".format(key))
         else:
             sync_file_down_if_stale(s3, key, metadata, bucket, root)
             sync_file_up_if_newer(s3, key, metadata, bucket, root)
+        logging.debug("Completed bi-directional sync for {}".format(key))
 
-        logging.info("Completed bi-directional sync for {}".format(key))
+
+def sync_manifest(manifest_filename, bucket, s3, root):
+    '''
+    Note:
+        Syncing the metadata also gets all of the locally cached information,
+        possibly reflecting the local state of another machine. Since we never
+        trust this cached info and always re-caclulate the local stats when syncing,
+        should we consider simply removing that cache of the local stats? Technically
+        speaking, it's only ever possible for it to be inaccurate while this program
+        is running, after syncing the remote manifest but before re-writing the manifest.
+    '''
+    remote_metadata = get_remote_metadata(s3, os.path.basename(manifest_filename), bucket)
+    metadata = {'s3_metadata': remote_metadata}
+    record_local_stats(root, manifest_filename, metadata)
+    bidirectionally_sync_file(os.path.basename(manifest_filename), metadata, bucket, root, s3)
 
 
 def sync_down_metadata(s3, manifest, k, v, bucket, root, manifest_filename):
-    try:
-        metadata = s3.head_object(Bucket = bucket, Key = k)
-        latest_metadata = {
-            'last-modified': timegm(time.strptime(metadata['ResponseMetadata']['HTTPHeaders']['last-modified'], '%a, %d %b %Y %H:%M:%S %Z')),
-            'etag': metadata['ResponseMetadata']['HTTPHeaders']['etag'].strip("'\""),
-            'content-length': metadata['ResponseMetadata']['HTTPHeaders']['content-length']
-            }
-    except EndpointConnectionError as e:
-        logging.error("Cannot make a connection: {}".format(e))
-        return
-    except Exception as e:
-        logging.error("Issue HEADing {} from {}; error {}, code {}".format(k, bucket, e, e.response['Error']['Code']))
-        if e.response['Error']['Code'] == '404':
-            latest_metadata = {'last-modified': 0, 'etag': None, 'context-length': None}
-        else:
-            return
+    latest_metadata = get_remote_metadata(s3, k, bucket)
 
-    if latest_metadata != v.get('s3_metadata', None):
+    if latest_metadata is None:
+        return
+    elif latest_metadata != v.get('s3_metadata', None):
         v['s3_metadata'] = latest_metadata
         write_json(manifest, manifest_filename)
         logging.info("Wrote new manifest data for {}.".format(k))
     else:
-        logging.info("Metadata unchanged for {}. Wrote nothing locally.".format(k))
+        logging.debug("Metadata unchanged for {}. Wrote nothing locally.".format(k))
+
+
+def get_remote_metadata(s3, key, bucket):
+    try:
+        metadata = s3.head_object(Bucket = bucket, Key = key)
+    except EndpointConnectionError as e:
+        logging.error("Cannot make a connection: {}".format(e))
+        return
+    except Exception as e:
+        logging.error("Issue HEADing {} from {}; error {}, code {}".format(key, bucket, e, e.response['Error']['Code']))
+        return
+
+    return {
+        'last-modified': timegm(time.strptime(metadata['ResponseMetadata']['HTTPHeaders']['last-modified'], '%a, %d %b %Y %H:%M:%S %Z')),
+        'etag': metadata['ResponseMetadata']['HTTPHeaders']['etag'].strip("'\""),
+        'content-length': metadata['ResponseMetadata']['HTTPHeaders']['content-length']
+        }
+
 
 def record_local_stats(root, key, metadata):
-    local_stats = os.stat(os.path.join(root, key))
-    metadata['local_last_modified'], metadata['local_size'] = local_stats.st_mtime, local_stats.st_size
-    metadata['local_etag'] = calculate_local_etag(os.path.join(root, key))
+    mtime, st_size, local_etag = calculate_local_stats(root, key)
+    metadata['local_last_modified'], metadata['local_size'] = mtime, st_size
+    metadata['local_etag'] = local_etag
+
+
+def calculate_local_stats(root, key):
+    try:
+        local_stats = os.stat(os.path.join(root, key))
+        return local_stats.st_mtime, local_stats.st_size, calculate_local_etag(os.path.join(root, key))
+    except FileNotFoundError as e:
+        logging.warn("Local file wasn't found. Returning 0, 0, None. It's probably a new file to download.")
+        return 0, 0, None
 
 
 def sync_file_down_if_stale(s3, k, v, bucket, root):
@@ -141,6 +180,10 @@ def write_json(data, path):
 def read_json(manifest_path):
     with open(manifest_path, 'r') as f:
         return json.load(f)
+
+
+def remote_and_local_hashes_are_equal(metadata):
+    return metadata['s3_metadata']['etag'] == metadata.get('local_etag')
 
 
 def calculate_local_etag(path):
