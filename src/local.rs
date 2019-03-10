ce284392990454ebc547a150865f4bbc19dd88af
Jordan McQueen <jordan@whoami.sh>
2019-03-10 04:14:26 -0700

    Implement local version of s3 e-tag MD5 hash algo

--- src/local.rs
+++ src/local.rs
@@ -1,6 +1,12 @@
+extern crate crypto;
+
+use crypto::digest::Digest;
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use std::io::{BufRead, BufReader};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+
+const S3_MD5_CHUNK_READ_LENGTH: usize = 10_240;
 
 /// Polls the local file system for changes in the relevant cloudhome directory
 /// paths. Upon receiving write events in the local filesystem, we spawn a task
@@ -36,4 +42,35 @@ pub fn poll_changes(paths: &Vec<String>) {
             Err(e) => println!("watch error: {:?}", e),
         }
     }
+}
+
+/// This is a proxy of S3s implementation to calculate the etag for single-part objects.
+
+/// Note that multi-part uploaded objects will hash differently than this
+/// function on S3s service, so we cannot rely on hash comparisons if the
+/// S3 object was multi-part uploaded.
+pub fn calculate_hash(file_path: &String) -> String {
+    let file = match std::fs::File::open(file_path) {
+        Ok(file) => file,
+        Err(_) => panic!("Error opening file {}", file_path),
+    };
+
+    // S3 uses a specific chunk length for computing the MD5 hash.
+    let mut reader = BufReader::with_capacity(S3_MD5_CHUNK_READ_LENGTH, file);
+    let mut digest = crypto::md5::Md5::new();
+
+    // Read sequential chunks of the file into a buffer, feeding the buffer
+    // to the digest. Rinse and repeat until there's no more file left.
+    loop {
+        let buffer = reader.fill_buf().expect("Problem filling buffer");
+        let length = buffer.len();
+        digest.input(buffer);
+
+        match length {
+            0 => break,
+            _ => reader.consume(length),
+        }
+    }
+
+    digest.result_str()
 }
